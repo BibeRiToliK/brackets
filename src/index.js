843249dceb4a9d0828f22f22f12aafc4abb3e51d
Uladzimir Piskunovich <vova.piskunovich2001@gmail.com>
2023-08-13 17:43:31 +0300

    feat: end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+const openBrackets = ["(", "{", "[","|"];
+const closeBrackets = [")","{","[","|"];
+let resArrOpenBrackets = [];
+let resArrCloseBrackets = [];
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   let chars = str.split('');
+   for (let i = 0; i < chars.length; i++) {
+     if (openBrackets.includes(chars[i])) {
+       resArrOpenBrackets.push(chars[i]);
+     }
+     if (closeBrackets.includes(chars[i])) {
+       resArrCloseBrackets.push(chars[i]);
+     }
+   }
+   return resArrCloseBrackets.length === resArrOpenBrackets.length
 }
